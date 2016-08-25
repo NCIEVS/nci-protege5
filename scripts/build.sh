e4326e1fd80d2bcd61b55d5c894b226a0538f209
Bob Dionne <dionne@dionne-associates.com>
2016-08-25 11:22:27 -0400

    Revert "Update build script to start using tagged branches"
    This reverts commit ab37946b109f1cdd4f7d998dca97825f7ad0c801.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,31 +5,30 @@ mkdir nci-protege5
 cd nci-protege5
 git clone https://github.com/bdionne/protege.git
 cd protege
-git checkout d1.4
+git checkout -b search-api origin/search-api
 mvn clean install -DskipTests=true
 cd ..
 git clone https://github.com/protegeproject/metaproject.git
 cd metaproject
-git checkout d1.4
 mvn clean install -DskipTests=true
 cp target/metaproject*.jar ../protege/protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/bundles
 cd ..
 git clone https://github.com/protegeproject/lucene-search-plugin.git
 cd lucene-search-plugin
-git checkout d1.4
+git checkout -b nci-lucene-search origin/nci-lucene-search
 mvn clean install -DskipTests=true
 cp target/lucene*.jar ../protege/protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/bundles
 cd ..
 git clone https://github.com/protegeproject/protege-server.git
 cd protege-server
-git checkout d1.4
+git checkout -b http-metaproject-integration origin/http-metaproject-integration
 mvn clean install -DskipTests=true
 cp target/protege-server*.jar ../protege/protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/bundles
 cp -R root target/server-distribution/server
 cd ..
 git clone https://github.com/protegeproject/protege-client.git
 cd protege-client
-git checkout d1.4
+git checkout -b http-metaproject-integration origin/http-metaproject-integration
 mvn clean install -DskipTests=true
 cp target/protege-client*.jar ../protege/protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/bundles
 
