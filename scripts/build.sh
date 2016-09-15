29c22d1dc414239f74eb100dbb7e14772f343b1d
Bob Dionne <dionne@dionne-associates.com>
2016-09-15 17:15:36 -0400

    Consolidated repos

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,41 +6,43 @@ mkdir nci-protege5
 cd nci-protege5
 git clone https://github.com/bdionne/protege.git
 cd protege
-git checkout -b search-api origin/search-api
+git checkout $TAG
 mvn clean install -DskipTests=true
 mkdir protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/plugins
 cp ../../run-editor.sh protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/run.sh
 cd ..
-git clone https://github.com/protegeproject/metaproject.git
+git clone https://github.com/bdionne/metaproject.git
 cd metaproject
 git checkout $TAG
 mvn clean install -DskipTests=true
 cp target/metaproject-1.0.0-SNAPSHOT.jar ../protege/protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/plugins
 cd ..
-git clone https://github.com/protegeproject/csv-export-plugin.git
+git clone https://github.com/bdionne/csv-export-plugin.git
 cd csv-export-plugin
+git checkout $TAG
 mvn clean install
 cp target/csv-export-plugin-1.0.0-SNAPSHOT.jar ../protege/protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/plugins
 cd ..
-git clone https://github.com/protegeproject/lucene-search-plugin.git
+git clone https://github.com/bdionne/lucene-search-plugin.git
 cd lucene-search-plugin
-git checkout -b nci-lucene-search origin/nci-lucene-search
+git checkout $TAG
 mvn clean install -DskipTests=true
 cp target/lucene-search-plugin-1.0.0-SNAPSHOT.jar ../protege/protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/plugins
 cd ..
-git clone https://github.com/protegeproject/lucene-search-tab.git
+git clone https://github.com/bdionne/lucene-search-tab.git
 cd lucene-search-tab
+git checkout $TAG
 mvn clean install
 cp target/lucene-search-tab-1.0.0-SNAPSHOT.jar ../protege/protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/plugins
 cd ..
-git clone https://github.com/protegeproject/protege-server.git
+git clone https://github.com/bdionne/protege-server.git
 cd protege-server
 git checkout $TAG
 mvn clean install -DskipTests=true
 cp target/protege-server*.jar ../protege/protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/plugins
 cp -R root target/server-distribution/server
 cd ..
-git clone https://github.com/protegeproject/protege-client.git
+git clone https://github.com/bdionne/protege-client.git
 cd protege-client
 git checkout $TAG
 mvn clean install -DskipTests=true
@@ -48,6 +50,7 @@ cp target/protege-client*.jar ../protege/protege-desktop/target/protege-5.0.1-SN
 cd ..
 git clone https://github.com/bdionne/nci-edit-tab.git
 cd nci-edit-tab
+git checkout $TAG
 mvn clean install -DskipTests=true
 cp target/nci-edit-tab*.jar ../protege/protege-desktop/target/protege-5.0.1-SNAPSHOT-platform-independent/Protege-5.0.1-SNAPSHOT/plugins
 cd ../
