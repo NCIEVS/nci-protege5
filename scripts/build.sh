8471c942ba5a244f5542e30af9216235055cf4b1
Bob Dionne <dionne@dionne-associates.com>
2022-03-17 13:52:59 -0400

    Updates to remove log4j classes

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,12 +27,16 @@ git clone https://github.com/$TARGET/protege.git
 cd protege
 git checkout $TAG
 mvn clean install -DskipTests=true
+zip -q -d protege-desktop/target/protege-5.1.2-SNAPSHOT-platform-independent/Protege-5.1.2-SNAPSHOT/lib/log4j-core-2.17.2.jar org/apache/logging/log4j/core/lookup/JndiLookup.class
+zip -q -d protege-desktop/target/protege-5.1.2-SNAPSHOT-platform-independent/Protege-5.1.2-SNAPSHOT/lib/log4j-core-2.17.2.jar org/apache/logging/log4j/core/appender/db/jdbc/JdbcAppender.class
 mkdir protege-desktop/target/protege-5.1.2-SNAPSHOT-platform-independent/Protege-5.1.2-SNAPSHOT/plugins
 cp ../../run-editor.sh protege-desktop/target/protege-5.1.2-SNAPSHOT-platform-independent/Protege-5.1.2-SNAPSHOT/run.sh
 cd protege-editor-owl/target/server-distribution
+zip -q -d server/lib/log4j-core-2.17.2.jar org/apache/logging/log4j/core/lookup/JndiLookup.class
+zip -q -d server/lib/log4j-core-2.17.2.jar org/apache/logging/log4j/core/appender/db/jdbc/JdbcAppender.class
 cp -R server ../../../../protege-server/target/server-distribution
 cd ../../..
-cp ../metaproject/target/metaproject-1.0.2-SNAPSHOT.jar ../protege/protege-desktop/target/protege-5.1.2-SNAPSHOT-platform-independent/Protege-5.1.2-SNAPSHOT/plugins
+cp ../metaproject/target/metaproject-1.0.3-SNAPSHOT.jar ../protege/protege-desktop/target/protege-5.1.2-SNAPSHOT-platform-independent/Protege-5.1.2-SNAPSHOT/plugins
 cd ../
 git clone https://github.com/$TARGET/explanation-workbench.git
 cd explanation-workbench
@@ -45,3 +49,4 @@ cp ../run-server.sh .
 zip -r nci-protege5-$NOW.zip protege run-server.sh run-protege.sh
 tar -cvzf nci-protege5-$NOW.tar.gz protege run-server.sh run-protege.sh
 rm -r $HOME/.m2/repository/org/apache/struts
+
